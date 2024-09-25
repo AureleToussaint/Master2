7ab9cae8b5c09486cd1a8ef42fd560115fce71d6
AureleToussaint <toussaint.aurele@gmail.com>
2024-09-25 09:08:56 +0200

    README

---
+++ README.md.m
@@ -0,0 +1,23 @@
+###############################################################################
+#                   Trait-based approach in Community Ecology
+#
+#
+#
+#                            Theory and applications
+#
+#
+#
+###############################################################################
+
+This 'GitHub' repository contain all ressources.
+
+- PPT of the course presentations
+- Associated R scripts to reproduce Figures and analyses presented in the property)
+
+- Material for group work.
+    - data
+    - R scripts
+    
+- Help for R training
+    - First steps in R. Basic R commands (for beginners)
+    - Common analyses in community analyses in R 
